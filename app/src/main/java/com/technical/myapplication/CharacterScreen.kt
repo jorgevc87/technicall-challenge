5a91e2ed602ed0133f9455b658c12f9fa098776e
JorgeVC87 <jorgevc_87@outlook.com>
2026-04-13 05:22:53 -0500

    ui

--- app/src/main/java/com/technical/myapplication/CharacterScreen.kt
+++ app/src/main/java/com/technical/myapplication/CharacterScreen.kt
@@ -1,13 +1,15 @@
 package com.technical.myapplication
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.compose.material3.*
+import com.technical.domain.CharacterModel
 
 @Composable
 fun CharacterScreen(viewModel: CharactersViewModel = hiltViewModel()) {
@@ -20,13 +22,27 @@ fun CharacterScreen(viewModel: CharactersViewModel = hiltViewModel()) {
         }
 
         is UIState.Error -> {
-            Column{
+            Column {
                 Text("")
             }
         }
 
         is UIState.Success -> {
-
+            val list = (state as UIState.Success).items
+            LazyColumn {
+                items(list) { item: CharacterModel ->
+                    Row {
+                        /*AsyncImage(
+                            model = item.image,
+                            contentDescription = item.name
+                        )*/
+                        Column {
+                            Text(item.name ?: "name")
+                            Text(item.status ?: "status")
+                        }
+                    }
+                }
+            }
         }
     }
 }
